37d55215bf78f319eb10a7ec9106129c07743d1f
Zsolt Gál <zsolt.gal@sessiondigital.com>
2022-12-02 12:52:01 +0100

    fix wrong interface

--- src/Renderer/DashboardRenderer.php
+++ src/Renderer/DashboardRenderer.php
@@ -3,8 +3,9 @@
 namespace Technodelight\Jira\Renderer;
 
 use Symfony\Component\Console\Output\OutputInterface;
+use Technodelight\Jira\Domain\DashboardCollection;
 
 interface DashboardRenderer
 {
-    public function render(OutputInterface $output, Collection $collection): void;
+    public function render(OutputInterface $output, DashboardCollection $collection): void;
 }
